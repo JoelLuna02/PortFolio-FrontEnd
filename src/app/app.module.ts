2610c9ba000fe8f10d7154b29e96e42e28c8ddbc
JoelLuna02 <hectorjoelluna1234@gmail.com>
2022-08-08 08:20:36 -0300

    Archivos del frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { CuerpoComponent } from './componentes/cuerpo/cuerpo.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CuerpoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
@@ -15,4 +21,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
